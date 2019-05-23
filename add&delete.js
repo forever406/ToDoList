bb655afc087293992bdfdadbbc0787638f0366ff
LJQ0526 <1064397388@qq.com>
2019-05-23 20:51:02 +0800

    增加删除

---
+++ add&delete.js
@@ -0,0 +1,39 @@
+$(document).ready(function () {
+    if(localStorage.hasOwnProperty("counter"))
+    {
+        var counter =localStorage.getItem("counter");
+        for(var i=0;i<counter;i++)
+        {
+            $(".todo").append(localStorage.getItem("key"+i));
+        }
+    }
+    else
+    {
+        counter=0;
+    }
+
+
+    $("#add").click(function () {
+        var data = $("#name").val();
+        var key = "key"+counter;
+        data = "<li id='"+key+"' class='item'>"+data+"</li>";
+        console.log(data);
+        $(".todo").append(data);
+
+
+         counter++;
+        console.log(key);
+        localStorage.setItem("counter",counter);
+        localStorage.setItem(key,data);
+        location.reload();
+    })
+
+    $(".item").dblclick(function (e) {
+         var athis=e.currentTarget.attributes[0].value;
+        $("#"+athis).remove();
+        localStorage.removeItem(athis);
+    })
+
+
+
+});
